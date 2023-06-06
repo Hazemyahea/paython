a7af392f6cedd2e788243246d76a73202bb05dc6
Hazem Yahea <32298139+Hazemyahea@users.noreply.github.com>
2023-06-06 06:27:17 +0300

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,30 +1,23 @@
-## binary Search With Python 
-my_list = [10, 20, 30, 40, 50]
-upper = len(my_list)
-lower = 0
-guees = 40
-mid = int((upper+lower) / 2)
-count = 0
+## Function to Make a binary search with parameter which you can add a list of numbers  
+def binary(my_list):
+    upper = len(my_list)
+    lower = 0
+    guees = 40
+    mid = int((upper+lower) / 2)
+    count = 0
+    while (upper >= lower):
+        mid = (upper + lower) // 2
+        print(mid)
+        count += 1
+        if my_list[mid] == guees:
+            print("We find guess after" + str(count) + "Try")
+            break
+        elif guees < my_list[mid]:
+                upper = mid
+                mid = int((upper+lower) / 2)
+        elif guees > my_list[mid]:
+                lower = mid
+                mid = int((upper+lower) / 2)
 
-while (upper >= lower):
-    mid = (upper + lower) // 2
-    print(mid)
-    count += 1
-    if my_list[mid] == guees:
-        print("We find guess after" + str(count) + "Try")
-        break
-    elif guees < my_list[mid]:
-            upper = mid
-            mid = int((upper+lower) / 2)
-    elif guees > my_list[mid]:
-            lower = mid
-            mid = int((upper+lower) / 2)
-
-
-#     if my_list[mid] == guees:
-#         print("We find guess after " + str(count) + " Try")
-#         break
-#     elif guees < my_list[mid]:
-#         upper = mid - 1
-#     else:
-#         lower = mid + 1
+## Example 
+binary([10, 20, 30, 40, 50])
